066da529540de54c4e1bccc2d3cbf636d9272e4a
surekhaenterprisesandtraders-cyber <surekhaenterprisesandtraders@gmail.com>
2026-06-28 21:33:25 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'onclicksolutions-v1';
+const urlsToCache = [
+  '/onclicksolutions/',
+  '/onclicksolutions/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request).catch(function() {
+        return caches.match('/onclicksolutions/index.html');
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
